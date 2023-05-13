2cd3794aa5ce09171e0b18b2078df48833e5b887
hamacekh <hamacekh@users.noreply.github.com>
2023-05-13 16:05:28 +0200

    Streamlit (#1)
    * First working streamlit version
    
    * Add download button
    
    * Input text fine tuning.

--- app/evaluation_chat.py
+++ app/evaluation_chat.py
@@ -3,11 +3,12 @@ from typing import List
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.schema import SystemMessage
 
-assignment_template = """Please evaluate student assignment according to criteria I will give you. Format the output as markdown.
+assignment_template = """Please evaluate student submission according to criteria I will give you.
+Format the output as markdown.
  Here are the criteria:
     {criteria}
     
-    Rest is the student assignment:
+    Rest is the student submission:
     {assignment}
 """
 
@@ -17,7 +18,10 @@ assignment_human: HumanMessagePromptTemplate = HumanMessagePromptTemplate.from_t
 def setup_chat():
     prompt = ChatPromptTemplate.from_messages([
         SystemMessage(
-            content="You are AI assistant teacher, that evaluates student assignments according to criteria"),
+            content="You are AI assistant teacher, that evaluates student submissions according to criteria."
+                    "You always include final grade or point summary in your evaluation, depending on what"
+                    "is applicable for the requested evaluation criteria."
+                    "You must not give non integral amount of points if not otherwise specified in the criteria."),
         assignment_human
     ])
     return prompt
--- app/main.py
+++ app/main.py
@@ -1,27 +1,55 @@
+import tempfile
+
+import streamlit as st
 from langchain import LLMChain
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredPDFLoader
+from streamlit.delta_generator import DeltaGenerator
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
-from app.evaluation_chat import setup_chat
-from app.streaming_file import StreamingFileCallbackHandler
+from evaluation_chat import setup_chat
+from streaming_widget import StreamingWidgetCallbackHandler
+from utils import get_filename_without_extension
+
+st.title("Student assignment evaluation")
 
 # llm = OpenAI(temperature=0, model_name="gpt-4", streaming=True, callbacks=[StreamingStdOutCallbackHandler()])
-chat = ChatOpenAI(temperature=0, model_name="gpt-4", streaming=True,
-                  callbacks=[StreamingStdOutCallbackHandler(), StreamingFileCallbackHandler("output/evaluation.md")])
 
-
-def main():
-    file_location = "test_input/single_document/Physics_IA.pdf"
-    loader = UnstructuredPDFLoader(file_location)
-    assignment_docs = loader.load()
+def execute_evaluation(student_file: UploadedFile, evaluation_criteria: UploadedFile,
+                       output_widget: DeltaGenerator) -> str:
+    chat = ChatOpenAI(temperature=0, model_name="gpt-4", streaming=True,
+                      callbacks=[StreamingWidgetCallbackHandler(output_widget)])
+    with tempfile.NamedTemporaryFile(suffix=".pdf") as fp:
+        fp.write(student_file.getvalue())
+        temp_path = fp.name
+        loader = UnstructuredPDFLoader(temp_path)
+        assignment_docs = loader.load()
     assignment = [doc.page_content for doc in assignment_docs]
-    print("\n\n".join(assignment))
-    with open("criteria/ib_marking_rubic.txt") as f:
-        criteria_text = f.read()
+    criteria_text = evaluation_criteria.getvalue().decode("utf-8")
     prompt = setup_chat()
     chain = LLMChain(llm=chat, prompt=prompt)
-    chain.run(criteria=criteria_text, assignment=assignment)
+    return chain.run(criteria=criteria_text, assignment=assignment)
+
+def main():
+    st.header("Input")
+    student_file = st.file_uploader(label="Upload student pdf assignment", type="pdf")
+    evaluation_criteria = st.file_uploader(label="Upload evaluation criteria as text file", type=["html", "md", "txt"])
+    if student_file is None or evaluation_criteria is None:
+        st.write("Pick student file and evaluation criteria")
+    else:
+        act = st.button("Evaluate")
+        if act:
+            st.header("Output")
+            output_widget = st.empty()
+            result_md = execute_evaluation(student_file, evaluation_criteria, output_widget)
+            output_widget.markdown(result_md)
+            student_file_basename = get_filename_without_extension(student_file.name)
+            st.download_button(
+                label="Download evaluation as markdown file",
+                data=result_md,
+                file_name=f"{student_file_basename}_evaluation.md",
+                mime="text/markdown"
+            )
 
 
 if __name__ == '__main__':
