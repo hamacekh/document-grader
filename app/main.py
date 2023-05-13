211ae00a37d8879318009c1ec8688297a41457bc
Jan Hamacek <jenda@hamacek.net>
2023-05-13 21:28:16 +0200

    Initial docx read implementation

--- app/main.py
+++ app/main.py
@@ -3,37 +3,45 @@ import tempfile
 import streamlit as st
 from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredFileLoader
 from streamlit.delta_generator import DeltaGenerator
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from evaluation_chat import setup_chat
 from streaming_widget import StreamingWidgetCallbackHandler
-from utils import get_filename_without_extension
+from utils import get_filename_without_extension, get_file_extension
 
-st.title("Student assignment evaluation")
+st.title("Student submission evaluation")
 
-# llm = OpenAI(temperature=0, model_name="gpt-4", streaming=True, callbacks=[StreamingStdOutCallbackHandler()])
+
+def uploaded_file_to_txt(uploaded: UploadedFile) -> str:
+    extension = get_file_extension(uploaded.name)
+    with tempfile.NamedTemporaryFile(suffix=f".{extension}") as fp:
+        fp.write(uploaded.getvalue())
+        temp_path = fp.name
+        loader = UnstructuredFileLoader(temp_path)
+        assignment_docs = loader.load()
+    assignment = "\n\n".join([doc.page_content for doc in assignment_docs])
+    return assignment
+
 
 def execute_evaluation(student_file: UploadedFile, evaluation_criteria: UploadedFile,
                        output_widget: DeltaGenerator) -> str:
     chat = ChatOpenAI(temperature=0, model_name="gpt-4", streaming=True,
                       callbacks=[StreamingWidgetCallbackHandler(output_widget)])
-    with tempfile.NamedTemporaryFile(suffix=".pdf") as fp:
-        fp.write(student_file.getvalue())
-        temp_path = fp.name
-        loader = UnstructuredPDFLoader(temp_path)
-        assignment_docs = loader.load()
-    assignment = [doc.page_content for doc in assignment_docs]
-    criteria_text = evaluation_criteria.getvalue().decode("utf-8")
+    submission = uploaded_file_to_txt(student_file)
+    criteria_text = uploaded_file_to_txt(evaluation_criteria)
     prompt = setup_chat()
     chain = LLMChain(llm=chat, prompt=prompt)
-    return chain.run(criteria=criteria_text, assignment=assignment)
+    return chain.run(criteria=criteria_text, submission=submission)
+
 
 def main():
     st.header("Input")
-    student_file = st.file_uploader(label="Upload student pdf assignment", type="pdf")
-    evaluation_criteria = st.file_uploader(label="Upload evaluation criteria as text file", type=["html", "md", "txt"])
+    student_file = st.file_uploader(label="Upload student pdf submission",
+                                    type=["pdf", "html", "htm", "md", "txt", "doc", "docx", "ppt", "pptx", "tex"])
+    evaluation_criteria = st.file_uploader(label="Upload evaluation criteria as text file",
+                                           type=["pdf", "html", "htm", "md", "txt", "doc", "docx", "ppt", "pptx", "tex"])
     if student_file is None or evaluation_criteria is None:
         st.write("Pick student file and evaluation criteria")
     else:
